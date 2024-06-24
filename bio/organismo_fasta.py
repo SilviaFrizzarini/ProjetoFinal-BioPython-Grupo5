604435805694f8970c8d64e4c12d41c9bbbbe226
YasminFranca <163673450+YasminFranca@users.noreply.github.com>
2024-06-24 19:48:40 -0300

    Update organismo_fasta.py

--- bio/organismo_fasta.py
+++ bio/organismo_fasta.py
@@ -1,5 +1,10 @@
+from sequencia import Sequencia
+
 class OrganismoFasta:
     def __init__(self, id, nome, sequencia):
         self.id = id
         self.nome = nome
         self.sequencia = Sequencia(sequencia)
+
+def mostrar_atributos(self):
+        print(f"ID: {self.id}, Nome: {self.nome}, Sequencia: {self.sequencia}")
